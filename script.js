811a9c012064a072b2e4d546c5fe5bc02ded741d
InjuMichorius <inju@hotmail.nl>
2023-10-28 17:13:31 +0200

    Add animaton effect on scroll

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ $(document).ready(function () {
     $(this).toggleClass("is-active");
   });
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+    // else {
+    //   entry.target.classList.remove(".show");
+    // }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
